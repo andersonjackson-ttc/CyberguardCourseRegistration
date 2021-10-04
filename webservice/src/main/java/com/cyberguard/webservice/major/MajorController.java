c74eb1398bd2d4f0a653c710a321e3954d0d4b44
arahimfahim40 <abdulrfahim.fahim@gmail.com>
2021-10-04 16:03:34 -0500

    Final Changes
    Fixed the dropdown. Everything is function now alhamdulillah.

--- webservice/src/main/java/com/cyberguard/webservice/major/MajorController.java
+++ webservice/src/main/java/com/cyberguard/webservice/major/MajorController.java
@@ -3,12 +3,13 @@ package com.cyberguard.webservice.major;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/v1/major")
+@RequestMapping(path = "/major")
 public class MajorController {
 	
 	//service variable
@@ -20,6 +21,7 @@ public class MajorController {
 		this.majorService = majorService;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping
 	public List<Major> getMajors() {
 		return majorService.getMajors();
