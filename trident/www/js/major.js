c574481a4b3ee3dca61fdf74bb502ae1de966651
AnthonyWGotz <90467200+AnthonyWGotz@users.noreply.github.com>
2021-10-19 13:18:23 -0400

    Add files via upload

--- trident/www/js/major.js
+++ trident/www/js/major.js
@@ -2,3 +2,26 @@ function menu() {
     var majorlist = document.getElementById("majorlist");
     document.getElementById("Favorite").value = majorlist.options[majorlist.selectedIndex].text;
   }
+
+window.onload = function()
+{
+    var btnMajor = document.getElementById('btnMajor');
+    
+    btnMajor.addEventListener('click', newTable, false);
+    
+}
+
+function newTable(){
+    clearcontent('table');
+    var checkbox = '<tr><td id="boxCourse"><input type="checkbox" class="taking"></td>';
+    var table = ' <table><tr><th id="boxCourse"></th><th id="textCourse">Course Id</th><th id="textCourse">Course Name</th></tr>';
+    table += checkbox + '<td id="textCourse">ABC-123</td><td id="textCourse">The Alphabet</td></tr></table>';
+    
+    document.getElementById('table').innerHTML = table;
+        
+    
+}
+
+function clearcontent(elementID) {
+            document.getElementById(elementID).innerHTML = "";
+        }
