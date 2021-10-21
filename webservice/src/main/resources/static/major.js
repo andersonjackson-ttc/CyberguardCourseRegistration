d7de81caddcfde88730a94ba097fe6a98551ddb2
arahimfahim40 <abdulrfahim.fahim@gmail.com>
2021-10-20 22:57:54 -0500

    Table, CSS, JavaScript function done
    Done with help of Anthony and Kamal.

--- webservice/src/main/resources/static/major.js
+++ webservice/src/main/resources/static/major.js
@@ -1,6 +1,6 @@
 var done=false;
 var submit = document.getElementById('submit');
-
+var courseLink = "";
 
 
 function getMajorList() {
@@ -9,12 +9,15 @@ function getMajorList() {
   urlString = "/major";
   $.ajax({method: "GET", url: urlString, dataType: 'json'})
     .done(function(response)  {
+     
       // response = JSON.stringify(response);
       //alert((JSON.stringify(response)));
       majorDropDown = $("#myDropdown");
       var appenddata1 = "";
-
+      
+      clearcontent('table');
         for (var i = 0; i < response.length; i++) {
+
             appenddata1 += "<option value = '" + response[i].id + " '>" + response[i].major_Name + " </option>";
         }
         $("#myDropdown").append(appenddata1);
@@ -32,11 +35,13 @@ function getMajorList() {
   }
 }
 
-// window.onload = function()
-// {
-//     submit.addEventListener('click', newTable, false);
+window.onload = function()
+{
+    var btnMajor = document.getElementById('btnMajor');
     
-// }
+    btnMajor.addEventListener('click', getCourses, false);
+    
+}
 
 function newTable(){
     clearcontent('table');
@@ -49,27 +54,53 @@ function newTable(){
     
 }
 
+
+//get the id of the dropdown
+function Class(str)
+{
+  courseLink = "/courses?major="+str.toString()+"+";
+  //courseLink =+ "/courses?majors=" /*+ str.toString()*/;
+  //alert(courseLink);
+}
+
 function getCourses() {
 
-  urlString = "/courses?major=7+";
-  $.ajax({method: "GET", url: urlString, dataType: 'json'})
+  $.ajax({method: "GET", url: courseLink, dataType: 'json'})
     .done(function(response)  {
       // response = JSON.stringify(response);
       //alert((JSON.stringify(response)));
       table = $("#table");
       var appenddata1 = "";
 
-        for (var i = 0; i < response.length; i++) {
-            for (var j=0; j<= i; j++)
-            {
 
-            }
-          //appenddata1 += "<td value = '" + response[i].id + " '>" + response[i].major_Name + " </option>";
-        }
-        $("#myDropdown").append(appenddata1);
-        done=true;
-      
-      
+
+      // for (var i = 0; i < response.length; i++) {
+      //     appenddata1 += "<tr>";
+      //       for (var j=0; j< i+1; j++)
+      //       {
+      //         appenddata1+= "<td id='boxCourse'><input type='checkbox' class='taking'>" + "</td>" + response[i].course_ID+"</td>" + "<td>"+ response[i].course_Name + "</td>";
+      //       }
+          
+      //   }
+      //   appenddata1 += "</tr>"
+      //   table.append(appenddata1);
+
+
+      appenddata1+="<table>"
+      appenddata1 += "<tr><th id='boxCourse'></th><th id='textCourse'>Course Id</th><th id='textCourse'>Course Name</th></tr>"
+      clearcontent('table');
+
+      for (var j=0; j< response.length; j++)
+      {
+        appenddata1+= "<tr><td id='boxCourse'><input type='checkbox' class='taking'></td>" + "<td id='textCourse'>" + response[j].course_ID+ "</td>" + "<td id='textCourse'>"+ response[j].course_Name + "</td></tr>";
+      }
+      appenddata1+="</table>"
+      table.append(appenddata1);
+  
+
+
+
+
     })
     .fail(function(){
       alert("Error connecting to the server");
@@ -79,6 +110,21 @@ function getCourses() {
     })
   ;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+  function clearcontent(elementID) {
+            document.getElementById(elementID).innerHTML = "";
+        }
 
 
 
